3b33b1dd2b4e50f5a6ffdf2ed8cc8aca5f5880c2
Adarsh Tadimari <ee12b003@smail.iitm.ac.in>
2015-05-04 20:30:51 +0530

    Iterative RANSAC implemented

--- code/detect_planes.m
+++ code/detect_planes.m
@@ -11,4 +11,43 @@ points_in_plane = points(inliers, :);
 hold on;
 plot3(points_in_plane(:, 1), points_in_plane(:, 2), points_in_plane(:, 3), 'r.')
 
+fprintf('Plane detection done..\n');
+disp(size(inliers, 2))
+
+i = 1
+while size(inliers, 2) >= 1000
+    %Running once more to get a second plane
+    inlier_index = 1;
+    global_index = 1;
+    outlier_index = 1;
+    outliers = ones(1, size(points, 1) - size(inliers, 2));
+
+    while global_index ~= size(points, 1) 
+        if inlier_index <= size(inliers, 2) 
+            if inliers(inlier_index) == global_index
+                inlier_index = inlier_index + 1;
+                global_index = global_index + 1;
+            else
+                outliers(outlier_index) = global_index;
+                outlier_index = outlier_index + 1;
+                global_index = global_index + 1;
+            end
+        else 
+            outliers(outlier_index) = global_index;
+            outlier_index = outlier_index + 1;
+            global_index = global_index + 1;
+        end
+    end
+
+    points = points(outliers, :);
+    % Run RANSAC to find inliers
+    [p, inliers] = ransac(points', @plane_3d, @plane_dist_3d, @isdegenerate, 3, 0.01);
+    points_in_plane = points(inliers, :);
+    plot3(points_in_plane(:, 1), points_in_plane(:, 2), points_in_plane(:, 3), 'g.')
+
+    fprintf('Plane detection done.., %d\n', i);
+    disp(size(inliers, 2));
+    i = i + 1;
+end
+
 
