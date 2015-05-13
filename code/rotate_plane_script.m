c2c3f46ec4c7da450c12881bf8492a36372bebed
Abhinav Garlapati <abhinav2710@gmail.com>
2015-05-13 21:29:36 +0530

    Visualize filled planes

--- code/rotate_plane_script.m
+++ code/rotate_plane_script.m
@@ -19,15 +19,16 @@
 % %Coor_2D = Coor_2D(1:3,:)';
 
 no_planes = size(new_planes.keys, 2);
-for i = 1:1
+keys = new_planes.keys();
+figure;
+hold all;
+for i = 1:no_planes    
     curr_plane = new_planes(keys{i});
     curr_plane = convert_2D(curr_plane);
     %curr_plane.points_2d = points_2d;
     %curr_plane.T = Transform;
-    figure;
-    plot(curr_plane.points_2d(:, 1), curr_plane.points_2d(:, 2), '.');
-    figure;
-    plot3(curr_plane.points(:, 1), curr_plane.points(:, 2), curr_plane.points(:, 3), '.');
+%     figure;
+%     plot(curr_plane.points_2d(:, 1), curr_plane.points_2d(:, 2), '.');
     
     points_2d = curr_plane.points_2d(:, 1:2);
     min_x = min(points_2d(:, 1));
@@ -41,9 +42,9 @@ for i = 1:1
     G = 1000;
     image = zeros(G, G);
     centered_points_2d = G * centered_points_2d;
-    for i = 1:size(centered_points_2d, 1)
-        x = floor(centered_points_2d(i, 1));
-        y = floor(centered_points_2d(i, 2));
+    for j = 1:size(centered_points_2d, 1)
+        x = floor(centered_points_2d(j, 1));
+        y = floor(centered_points_2d(j, 2));
         if y > 0
             image(x+1, y) = 1;
         end
@@ -67,21 +68,21 @@ for i = 1:1
     end
     se = strel('square',5);
     image = imdilate(image, se);
-    image = imfill(image);
+    %image = imfill(image);
     image = imdilate(image, se);
-    image = imfill(image);
+    %image = imfill(image);
     se = strel('square',3);
     image = imdilate(image, se);
-    image = imfill(image);
+    %image = imfill(image);
     
     
     new_nPts = sum(sum(image));
     new_2d_points = zeros(new_nPts, 2);
     counter = 1;
-    for i = 1:G
+    for l = 1:G
         for j = 1:G
-            if image(i, j) > 0
-                new_2d_points(counter, :) = [i-1, j-1];
+            if image(l, j) > 0
+                new_2d_points(counter, :) = [l-1, j-1];
                 counter = counter + 1;
             end
         end
@@ -94,5 +95,12 @@ for i = 1:1
     new_2d_points_homo = [new_2d_points, zeros(new_nPts, 1), ones(new_nPts, 1)];
     new_3d_points = curr_plane.T * new_2d_points_homo';
     new_3d_points = new_3d_points';
-    
+    curr_plane.filled_3d = new_3d_points;
+    %close all;
+    %figure;
+    %plot3(curr_plane.points(:, 1), curr_plane.points(:, 2), curr_plane.points(:, 3), '.');
+    %figure;
+    plot3(curr_plane.filled_3d (:, 1), curr_plane.filled_3d (:, 2), curr_plane.filled_3d (:, 3), '.');
+    %waitforbuttonpress;
+    new_planes(keys{i}) = curr_plane;
 end
